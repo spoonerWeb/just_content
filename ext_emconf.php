3ed857423cf2d5093300106841572343cf8090fc
Thomas Löffler <loeffler@spooner-web.de>
2017-01-31 21:01:50 +0100

    [TASK] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Just Content - plan and assign content updates',
+    'description' => 'Assign content and pages to backend users to prevent outdated content.',
+    'category' => 'fe',
+    'version' => '0.0.7',
+    'state' => 'beta',
+    'author' => 'Thomas Löffler',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.0-8.99.99',
+            'fluid_styled_content' => '7.6.0'
+        ],
+        'conflicts' => [],
+        'suggests' => []
+    ]
+];
